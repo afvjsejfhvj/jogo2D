fa59e985c9c5527a4922bae1229c561964b51ecd
afvjsejfhvj <mateusmbiscaro08@gmail.com>
2025-03-05 09:19:51 -0300

    iniciando js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-a=1
+//pegar elemento canvas pelo id
+const canvas = document.getElementById('jogo2D')
+// inicializar o canvas
+const ctx = canvas.getContext('2D')
+//criar função loop 60 hz
+function loop() {
+
+}
+
+loop()
