074a01d0839deb5afb2cfa732d5f8137e26141f3
afvjsejfhvj <mateusmbiscaro08@gmail.com>
2025-03-10 09:53:28 -0300

    obstaculo e colisao

--- script.js
+++ script.js
@@ -6,13 +6,19 @@ const ctx = canvas.getContext('2d')
 const gravidade = 1 
 
 
+
 document.addEventListener('keypress', (e) => {
-    if(e.code == 'Space' && personagem.pulando == false){
+    if(e.code == 'KeyW' && personagem.pulando == false){
         personagem.velocidadey = 20
         personagem.pulando = true
-    }
-})
+    }}
+)
 
+document.addEventListener('click', (E)=> {
+ if(gameover == true){
+    location.reload()
+ }
+})
 
 
 
@@ -25,12 +31,12 @@ const personagem = {
     pulando: false,
 } 
 
-
 function desenharPersonagem(){
     ctx.fillStyle = 'silver'
     ctx.fillRect(personagem.x,personagem.y,personagem.altura,personagem.largura)
 
 }
+
 function atualizarPersonagem(){
     if(personagem.pulando == true){
         personagem.velocidadey -= gravidade
@@ -40,28 +46,78 @@ function atualizarPersonagem(){
             personagem.pulando = false
             personagem.y = canvas.height-50
         }
-        
+    }  
+}
 
-    }   
+const Obstaculo = {
+    x: canvas.width - 50,
+    y: canvas.height - 100,
+    largura: 50,
+    altura: 100,
+    velocidadex: 5
+}
 
+function desenharObstaculo(){
+    ctx.fillStyle = 'orange'
+    ctx.fillRect(Obstaculo.x,Obstaculo.y,Obstaculo.largura,Obstaculo.altura )
+}
+
+function atualizaObstaculo(){
+    Obstaculo.x -= Obstaculo.velocidadex
+    if(Obstaculo.x <= 0 - Obstaculo.largura){
+        Obstaculo.x = canvas.width
+        Obstaculo.velocidadex += 0.2
+        let nova_altura = (Math.random() * 50) + 100
+        Obstaculo.altura = nova_altura
+        Obstaculo.y = canvas.height - nova_altura 
+    }
+}
+
+function HouveColisao(){
+    personagem.velocidadey = 0 
+    Obstaculo.velocidadex = 0 
+    ctx.fillStyle = 'red'
+    ctx.fillRect((canvas.width/2)-200,(canvas.height/2)-50, 400,100)
+    ctx.fillStyle='black'
+    ctx.font = '50px Arial'
+    ctx.fillText("GAME OVER", (canvas.width/20) + 250, (canvas.height/20) + 200, 200, 600)
+    gameover = true
 
 }
+
+function verificaColisao(){
+    if(personagem.x < Obstaculo.x + Obstaculo.largura && 
+       personagem.x + personagem.largura > Obstaculo.x &&  
+       personagem.y < Obstaculo.y + Obstaculo.altura && 
+       personagem.y + personagem.altura > Obstaculo.y
+    ){
+        HouveColisao()
+    }
+
+}
+
 
 
 
 //criar função loop 60 hz
 function loop() {
+    if(gameover = true){
     //apaga o de antes
     ctx.clearRect(0,0,canvas.width,canvas.height)
     //desenha denovo
     desenharPersonagem()
     //atualiza posições 
+    verificaColisao()
+    
     atualizarPersonagem()
-
-
+    desenharObstaculo()
+    atualizaObstaculo()
     //chama loop denovo
     requestAnimationFrame(loop)
+    }
 
 }
+
+
 
 loop()
